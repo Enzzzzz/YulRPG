b7fc0bbea53e538e00fa5263e7950693573ec2f9
Enzzzzz <contatoenzz@gmail.com>
2023-01-20 23:46:58 -0300

    Getting MongoDB URL from .env file

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,8 +1,7 @@
-const client = require('..');
 const chalk = require('chalk');
 const ms = require('ms');
-const mongoose = require("mongoose")
-const { Database } = require("../config/condig.json")
+const mongoose = require('mongoose');
+const client = require('..');
 
 /**
  * @event 'ready'
@@ -12,37 +11,39 @@ const { Database } = require("../config/condig.json")
  * Ele também registra uma mensagem no console para indicar que o bot está online.
  */
 client.on('ready', () => {
-    const { user, ws } = client;
-    /**
-     * @function setInterval
-     * Ele define um intervalo para atualizar a atividade do bot para exibir o ping atual em milissegundos a cada 5 segundos.
-     *
-     * @param {function} função a ser executada a cada 5 segundos
-     * @param {number} 5s - tempo em milissegundos para o intervalo
-     *
-     * @returns {number} um timerId
-     *
-     */
-    setInterval(() => {
-        user.setActivity({ name: "Comece agora, digite /RpgStart", type: 3 });
-    }, ms('5s'));
+  const { user } = client;
+  /**
+   * @function setInterval
+   * Ele define um intervalo para atualizar a atividade do bot para exibir o ping atual em milissegundos a cada 5 segundos.
+   *
+   * @param {function} função a ser executada a cada 5 segundos
+   * @param {number} 5s - tempo em milissegundos para o intervalo
+   *
+   * @returns {number} um timerId
+   *
+   */
+  setInterval(() => {
+    user.setActivity({ name: 'Comece agora, digite /RpgStart', type: 3 });
+  }, ms('5s'));
 
-    /**
-     * @function console.log
-     * Registra uma mensagem no console para indicar que o bot está online.
-     *
-     * @param {string} `${client.user.username} online!` - mensagem a ser registrada no console
-     */
+  /**
+   * @function console.log
+   * Registra uma mensagem no console para indicar que o bot está online.
+   *
+   * @param {string} `${client.user.username} online!` - mensagem a ser registrada no console
+   */
 
-    if(!Database) return;
-    mongoose.connect(Database, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        console.log(chalk.green("[Sucesso] Conectado com a Database."))
-    }).catch((err) => {
-        console.log(err)
+  mongoose
+    .connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
+    .then(() => {
+      console.log(chalk.green('[Sucesso] Conectado com a banco de dados (MongoDB).'));
+    })
+    .catch((err) => {
+      console.log('[Error] Não conectei com o banco de dados (MongoDB). ', err);
+    });
 
-    console.log(chalk.red(`${client.user.username} online!`));
+  console.log(chalk.red(`${client.user.username} online!`));
 });
